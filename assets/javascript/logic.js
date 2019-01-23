33df56dbed42c4595e1fcad3d30834e2e2f95a91
Jamie DeLong <jdlong1980@gmail.com>
2019-01-22 23:16:25 -0500

    updated all files

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -3,8 +3,8 @@ var genre;
 var queryURL;
 var page = 1;
 var totalPages;
-var song = document.createElement("audio");
-var file = "https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/Music/v4/dc/8b/a8/dc8ba869-69c7-7d29-f108-b7540c938139/mzaf_811735733402692469.plus.aac.p.m4a";
+var songPreview = document.createElement("audio");
+var soundtrackSongs = {};
 var genres = { "Blues": 2, "Comedy": 3, "Children's Music": 4, "Classical": 5, "Country": 6, "Electronic": 7, "Holiday": 8, "Opera": 9, "Jazz": 11, "Latino": 12, "New Age": 13, "Pop": 14, "R&B/Soul": 15, "Soundtrack": 16, "Dance": 17, "Hip-Hop/Rap": 18, "World": 19, "Alternative": 20, "Rock": 21, "Christian & Gospel": 22, "Vocal": 23, "Raggae": 24, "Easy Listening": 25, "J-Pop": 27, "Anime": 29, "K-Pop": 51, "Instrumental": 53, "Brazillian": 1122, "Disney": 50000063 }
 
 function displayMovies(response) {
@@ -31,17 +31,23 @@ function displayMovies(response) {
         $(".movie-count").text(" - 0 movies found");
     };
 
-    $(".movie").fadeIn("slow", function() {
+    $(".movie").fadeIn("slow", function () {
         //animation complete
     });
 };
 
 function getMovieSoundtrack() {
+    //Determine if movie is already selected and stop if it is
+    if (term === $(this).attr("data-title")) { return false };
+
     $(".soundtrack-count").text(" - Searching...");
     $(".movie").attr("style", "border: 1px solid black;");
     $(this).attr("style", "border: 4px solid yellow;");
 
     $("#movie-search-soundtracks").empty();
+    $("#soundtrack-table").empty();
+    $("#soundtrack-header").text("Soundtrack");
+
     term = $(this).attr("data-title");
     queryURL = "https://itunes.apple.com/search?term=" + term.replace(/\W+/g, '+').toLowerCase() + "&limit=200&media=music&entity=album&country=us&genreId=" + genres.Soundtrack;
     console.log(queryURL);
@@ -49,7 +55,7 @@ function getMovieSoundtrack() {
         url: queryURL,
         method: "GET",
         dataType: "JSON"
-    }).then(function(response) {
+    }).then(function (response) {
         console.log(response);
         var soundtracks = response.results;
         var albumIDS = [];
@@ -67,14 +73,11 @@ function getMovieSoundtrack() {
                 url: queryURL,
                 method: "GET",
                 dataType: "JSON"
-            }).then(function(response) {
+            }).then(function (response) {
                 console.log(response);
                 displayMovieSoundtrack(response)
             });
         } else {
-            $('#modal-title').text("Soundtrack(s)");
-            $('#modal-message').text("No soundtracks were found for the selected movie.");
-            $('#modal-box').modal('show');
             $(".soundtrack-count").text(" - 0 soundtrack(s) found");
         };
     });
@@ -87,7 +90,7 @@ function displayMovieSoundtrack(response) {
             if (imgURL) {
                 var image = $("<img class='img-fluid' alt='Image Unavailable'>").attr("src", imgURL);
                 var soundtrackDiv = $("<div class='movie-soundtrack'>");
-                soundtrackDiv.attr("data-id", response.results.collectionId);
+                soundtrackDiv.attr("data-id", response.results[i].collectionId);
                 soundtrackDiv.append(image);
                 soundtrackDiv.hide();
                 $("#movie-search-soundtracks").append(soundtrackDiv);
@@ -98,14 +101,30 @@ function displayMovieSoundtrack(response) {
     var soundTrackCount = $("#movie-search-soundtracks").children().length;
     $(".soundtrack-count").text(" - " + soundTrackCount + " soundtrack(s) found");
 
-    $(".movie-soundtrack").fadeIn("slow", function() {
+    $(".movie-soundtrack").fadeIn("slow", function () {
         // animation complete
     });
+
+    soundtrackSongs = response.results;
 };
 
-function displayAlbumTracks() {
+function displaySoundtrackSongs() {
     $(".movie-soundtrack").attr("style", "border: 1px solid black;");
     $(this).attr("style", "border: 4px solid yellow;");
+
+    $("#soundtrack-table").empty();
+    for (var i = 0; i < soundtrackSongs.length; i++) {
+        if (soundtrackSongs[i].wrapperType === "track" && soundtrackSongs[i].collectionId.toString() === $(this).attr("data-id")) {
+            $("#soundtrack-header").text(soundtrackSongs[i].collectionName);
+            var newTR = $('<tr>');
+            newTR.append('<td><img class="img-fluid play-button" src="assets/images/play.png" data-preview=' + soundtrackSongs[i].previewUrl + '></td>');
+            newTR.append('<td>' + soundtrackSongs[i].discNumber + '</td>');
+            newTR.append('<td>' + soundtrackSongs[i].trackNumber + '</td>');
+            newTR.append('<td>' + soundtrackSongs[i].trackName + '</td>');            
+            newTR.append('<td><a href=' + soundtrackSongs[i].artistViewUrl + ' target="_blank">'+ soundtrackSongs[i].artistName + '</a></td>');
+            $('#soundtrack-table').append(newTR);
+        }
+    }
 };
 
 function resetMovieSearch() {
@@ -114,6 +133,8 @@ function resetMovieSearch() {
     $("#movie-search-soundtracks").empty();
     $(".movie-count").text("");
     $(".soundtrack-count").text("");
+    $("#soundtrack-header").text("Soundtrack");
+    $("#soundtrack-table").empty();
 }
 
 function searchTMDB() {
@@ -123,7 +144,7 @@ function searchTMDB() {
         url: queryURL,
         method: "GET",
         dataType: "JSON"
-    }).then(function(response) {
+    }).then(function (response) {
         console.log(response);
         totalPages = response.total_pages;
         var totalResults = response.total_results;
@@ -137,7 +158,17 @@ function searchTMDB() {
     });
 };
 
-$("#search-button").on("click", function(event) {
+$("#search-by").change(function () {
+    if ($("#search-by").val() === "Music Genre") {
+        $("#genre-group").show();
+        $("#search-term").prop("disabled", true);
+    } else {
+        $("#genre-group").hide();
+        $("#search-term").prop("disabled", false);
+    };
+});
+
+$("#search-button").on("click", function (event) {
     event.preventDefault();
     term = $("#search-term").val().trim();
     term = term.replace(/\W+/g, '+').toLowerCase();
@@ -148,26 +179,16 @@ $("#search-button").on("click", function(event) {
             page = 1; // start on page one of possible movie search results
             searchTMDB();
             break;
-
     };
-
 });
 
+// Search by Movie - begin the process of finding the soundtrack for the selected movie
 $(document).on("click", ".movie", getMovieSoundtrack);
-$(document).on("click", ".movie-soundtrack", displayAlbumTracks);
-$("#search-by").change(function() {
-    if ($("#search-by").val() === "Music Genre") {
-        $("#genre-group").show();
-        $("#search-term").prop("disabled", true);
-    } else {
-        $("#genre-group").hide();
-        $("#search-term").prop("disabled", false);
-    };
+
+// Display the songs for the selected soundtrack
+$(document).on("click", ".movie-soundtrack", displaySoundtrackSongs);
+
+$(document).on("click", ".play-button", function(){
+    songPreview.setAttribute("src", $(this).attr("data-preview"));
+    songPreview.play();
 });
-
-// $("#play").on("click", function () {
-//     song.setAttribute("src", file);
-//     song.play();
-// })
-
-// displayMusicInfo();
